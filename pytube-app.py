6a048dc6cd5bf1c3147bc6b236a8f33a3993180a
NinjaCof <jon.pecar@gmail.com>
2023-12-22 00:21:38 +0100

    Update pytube-app.py

--- pytube-app.py
+++ pytube-app.py
@@ -2,6 +2,10 @@ import pytube
 import os
 from tkinter import *
 import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog, Entry, Button
+from pathlib import *
+from pathlib import Path
 
 
 def main():
@@ -16,13 +20,8 @@ def main():
     print("URL:", url_value)
     print("PATH:", path_value)
 
-
     yt = pytube.YouTube(vnos.get())
-
-    if (vnos_type == "mp3"):
-        video = yt.streams.filter(only_audio=True).first()
-    else:
-        video = yt.streams.first()
+    video = yt.streams.first()
 
     print(yt.title)
 
@@ -30,24 +29,42 @@ def main():
 
     video.download(place.get())
 
-    x = Label(root, text="Končano")
-    x.pack()
-    x1 = Label(root, text="Vaš posnetek: "+yt.title+" je bil uspešno prenešen!")
-    x1.pack()
 
     print("Končano!")
 
+    if (mp3_mp4_value == "mp3"):
+        original_path = f"{path_value}/{yt.title}.3gpp"
+        print(original_path)
+        converted_path = original_path.replace("/", "\\")
+
+        p = Path(converted_path)
+        new_file = p.with_suffix(".mp3")
+        p.rename(new_file)
+        print(new_file)
+
+    vnos_type.delete(0, tk.END)
+    vnos.delete(0, tk.END)
+    place.delete(0, tk.END)
+
+
+def folder_selector():
+    folder = filedialog.askdirectory()
+    folderPath.set(folder)
+    place.delete(0, tk.END)
+    place.insert(0, folder)
 
 
 # Create the Tkinter window
 root = tk.Tk()
-root.geometry('800x400')  # Adjust the window size as needed
+root.geometry('800x500')  # Adjust the window size as needed
 root.title('Downloader')
+
+folderPath = tk.StringVar()
 
 # Declare variables to store the values
 mp3_mp4_var = tk.StringVar()
 url_var = tk.StringVar()
-path_var = tk.StringVar()
+# path_var = tk.StringVar()
 
 # Label and Entry for mp3/mp4
 vnosTypeTitle = Label(root, text="mp3/mp4: ", font=("Arial", 20))
@@ -67,12 +84,16 @@ vnos.grid(row=1, column=1, pady=10)
 placeTitle = Label(root, text="PATH: ", height=1, font=("Arial", 20))
 placeTitle.grid(row=2, column=0, pady=10)
 
-place = Entry(root, font=("Arial", 20), textvariable=path_var, width=30)
+place = Entry(root, font=("Arial", 20), textvariable=folderPath, width=30)
 place.grid(row=2, column=1, pady=10)
 
 # Download button
 download_button = Button(root, text="Download", command=main, height=5, width=25, font=("Arial", 20))
 download_button.grid(row=3, column=0, columnspan=2, pady=20)
+
+# PATH button
+path_button = Button(root, text="Folder Select", command=folder_selector)
+path_button.grid(row=4, column=0, pady=20)
 
 # Center the interface
 root.update_idletasks()
