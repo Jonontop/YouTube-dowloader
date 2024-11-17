871b2280ad6b2cbc221f0ba8b6440734df49bacc
NinjaCof <jon.pecar@gmail.com>
2024-11-17 21:17:14 +0100

    Update pytube.py

--- pytube.py
+++ pytube.py
@@ -1,22 +1,13 @@
-import pytube
-from pytube import YouTube
-import os
+import pytubefix as pytube
 
-
-vnos_type= input("Vnesi ali želiš mp3(zvok) ali mp4(zvok + video): ")
+vnos_type = input("Vnesi ali želiš mp3 (zvok) ali mp4 (zvok + video): ")
 vnos = input("Vnesi URL za video: ")
 place = input("Vnesi datotečno pot do želene mape: ")
 
 yt = pytube.YouTube(vnos)
 
-if (vnos_type == "mp3"):
-    video = yt.streams.filter(only_audio=True).first()
-else:
-    video = yt.streams.first()
+print(f"Naslov videa: {yt.title} \nPrenašanje...")
 
+yt.streams.filter(only_audio=True).first().download(place) if vnos_type == "mp3" else yt.streams.get_highest_resolution().download(place)
 
-print(yt.title)
-
-print("prenašanje...")
-
-video.download(place)
+print("Prenos končan!")
